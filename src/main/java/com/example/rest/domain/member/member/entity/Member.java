4cde2cd9a1fd1043551a25cef0e8984f0a1dd81d
rOyalFruit <ekdeon@gmail.com>
2024-12-28 02:11:39 +0900

    회원 샘플 데이터 추가

---
+++ src/main/java/com/example/rest/domain/member/member/entity/Member.java
@@ -0,0 +1,21 @@
+package com.example.rest.domain.member.member.entity;
+
+import com.example.rest.global.jpa.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseTime {
+    @Column(unique = true, length = 30)
+    private String username;
+    @Column(length = 50)
+    private String password;
+    @Column(length = 30)
+    private String nickname;
+}
---
+++ src/main/java/com/example/rest/domain/member/member/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.example.rest.domain.member.member.service;
+
+import com.example.rest.domain.member.member.entity.Member;
+import com.example.rest.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    public long count() {
+        return memberRepository.count();
+    }
+    public Member join(String username, String password, String nickname) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .nickname(nickname)
+                .build();
+        return memberRepository.save(member);
+    }
+}
